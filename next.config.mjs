75b172c6de6d906f4a8fa9942cbfef8287a7203a
Gaurang <156237432+GaurangKhanderay@users.noreply.github.com>
2025-06-28 20:42:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
